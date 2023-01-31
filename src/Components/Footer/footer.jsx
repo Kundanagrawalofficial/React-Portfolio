e45aca310571d0dc554aa584c8e823c62bf5be01
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-01-31 08:42:04 +0530

    Update footer.jsx

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -6,7 +6,7 @@ import {IoLogoTwitter} from "react-icons/io"
 const Footer = () => {
   return (
   <footer>
-    <a href="#" className="footer__logo"></a>
+    <a href="https://kundan-react-portfolio.netlify.app/" className="footer__logo"></a>
     <ul className='permalinks'>
     <li><a href='#'>Home</a></li>
     <li><a href='#about'>About</a></li>
